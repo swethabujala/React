2cf23f4afe7b832cbc564594b0482c026d7b3e43
swetha <bujalaswetha5@gmail.com>
2023-12-31 15:53:13 +0530

    thankyou sulthana

--- myfirst/src/App.js
+++ myfirst/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import './App.css';
+import Swetha from "./swetha/swetha";
+
 function App() {
   return (
     <div>
     <h2>hello</h2>
+    <Swetha/>
     </div>
   );
 }
---
+++ myfirst/src/swetha/swetha.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Swetha() {
+    return (
+        <div>
+            <h1>hii sulthana</h1>
+        </div>
+    )
+}
+
+export default Swetha
